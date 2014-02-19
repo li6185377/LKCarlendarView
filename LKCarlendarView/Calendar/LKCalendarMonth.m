583f2136ebabc201ea4f46a763bedb56a52cb76a
li6185377 <linyunfeng@ljh-341.local>
2014-02-19 12:08:25 +0800

    add remark

--- LKCarlendarView/Calendar/LKCalendarMonth.m
+++ LKCarlendarView/Calendar/LKCalendarMonth.m
@@ -80,15 +80,22 @@
     NSDateComponents* dateComponents = [_currentMonth copy];
     dateComponents.day = 1;
     
+    //获得当前月的第一天时间
     NSDate* firstDay =  [currentLKCalendar dateFromComponents:dateComponents];
+    
+    //获得第一天 是周几
     int firstWeekDay = [currentLKCalendar components:NSWeekdayCalendarUnit fromDate:firstDay].weekday;
+    
+    //获取第一天 在日历中的位置。。     （根据你的设置去换算周几）
     int firstDayPosition = (firstWeekDay - _firstDayWeek + 8)%8;
+    
+    //第一行第一天  跟  当前月第一天    的差距天数
     int dayDiff = 1 - firstDayPosition + 1;
     
     int size = _dayViews.count;
     NSMutableArray* dateArray = [NSMutableArray arrayWithCapacity:size];
     for (int i=0;i<size; i++) {
-        
+        //剩下的简单了  将第一行第一天  不断的加一  然后 保存起来  就可以 获得整个月的 时间集合了
         dateComponents.day = dayDiff;
         NSDate* date = [currentLKCalendar dateFromComponents:dateComponents];
         [dateArray addObject:date];
@@ -98,6 +105,7 @@
     [self.dates removeAllObjects];
     self.dates = [NSMutableArray arrayWithArray:dateArray];
     
+    //有时间 集合了 剩下的 就是显示了
     [self refreshMonthView];
 }
 -(void)refreshMonthView
