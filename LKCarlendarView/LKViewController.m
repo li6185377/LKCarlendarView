0aae082dcbf2352c7b1bbceb81586ffd45af85fb
li6185377 <linyunfeng@ljh-341.local>
2014-02-27 10:38:46 +0800

    加入了 随机  周几开始
    demo

--- LKCarlendarView/LKViewController.m
+++ LKCarlendarView/LKViewController.m
@@ -14,6 +14,7 @@
 @property(weak,nonatomic)LKCalendarDayView* lastSelectedDayView;
 
 @property(strong,nonatomic)UILabel* lb_show;
+@property(strong,nonatomic)UIView* weekNameView;
 @end
 
 @implementation LKViewController
@@ -21,8 +22,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.weekNameView = [[UIView alloc]initWithFrame:CGRectMake(0, 20, 320, 30)];
+    [self.view addSubview:_weekNameView];
+    
     self.calendarView = [[LKCalendarView alloc]initWithFrame:CGRectMake(0, 50, 320, SYCC_MonthHeight)];
-    _calendarView.backgroundColor = [UIColor grayColor];
+    
+    [self createWeekName:(arc4random()%7)];
+    
     _calendarView.currentDateComponents = getYearMonthDateComponents([NSDate date]);
     [_calendarView startLoadingView];
     
@@ -37,6 +44,47 @@
 
     [self calendarViewDidChangedMonth:_calendarView];
 }
+
+-(void)createWeekName:(int)tag
+{
+    NSMutableArray* array = [NSMutableArray arrayWithArray:@[@"日",@"一",@"二",@"三",@"四",@"五",@"六"]];
+
+    NSMutableArray* showArray = [NSMutableArray array];
+    int index = tag;
+    while (showArray.count<7) {
+        [showArray addObject:[array objectAtIndex:index%7]];
+        index ++;
+    }
+    
+    while (_weekNameView.subviews.count) {
+        [[_weekNameView.subviews objectAtIndex:0] removeFromSuperview];
+    }
+    
+    _calendarView.leftMonth.firstDayWeek = tag;
+    _calendarView.centerMonth.firstDayWeek = tag;
+    _calendarView.rightMonth.firstDayWeek = tag;
+    
+    int i =0;
+    for (NSString* title in showArray) {
+        UILabel* lb = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 20,14)];
+        lb.textAlignment = NSTextAlignmentCenter;
+        lb.center = CGPointMake(SYCC_DayOffsetWidth + (SYCC_DayOffsetWidth+SYCC_DayWidth)*i  + SYCC_DayWidth/2, 30/2) ;
+        lb.backgroundColor = [UIColor clearColor];
+        lb.font = [UIFont systemFontOfSize:12];
+        if([title isEqualToString:@"六"] || [title isEqualToString:@"日"])
+        {
+            lb.textColor = [UIColor redColor];
+        }
+        else
+        {
+            lb.textColor = [UIColor blackColor];
+        }
+        lb.text = title;
+        [_weekNameView addSubview:lb];
+        i++;
+    }
+}
+
 -(void)calendarViewDidChangedMonth:(LKCalendarView *)sender
 {
     _lb_show.text = [NSString stringWithFormat:@"%d年%d月",sender.currentDateComponents.year,sender.currentDateComponents.month];
@@ -44,6 +92,7 @@
 -(void)calendarMonth:(LKCalendarMonth *)month dayView:(LKCalendarDayView *)dayView date:(NSDate *)date
 {
    int monthdiff = monthDiffWithDateComponents(month.currentMonth,getYearMonthDateComponents(date));
+    dayView.backgroundColor = [UIColor grayColor];
     dayView.date = date;
     dayView.lb_date.hidden = (monthdiff != 0);
 }
